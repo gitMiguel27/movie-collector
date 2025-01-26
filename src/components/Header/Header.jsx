be525379314cce540a1bdae49f2fe191cff6e7b8
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-26 17:34:27 -0500

    updated navbar styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Movie Collector</Navbar.Brand>
+        <Navbar.Brand className="mx-auto" href="#home">Movie Collector</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       </Container>
     </Navbar>
