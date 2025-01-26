a4c77f2101a0ccd8dc6b667160ac07ec16be12c0
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-26 15:28:00 -0500

    updated useEffect to not render undefined movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Form from './components/Form/Form'
-import MovieCard from './components/MovieCard/MovieCard'
 import MovieList from './components/MovieList/MovieList'
 
 function App() {
   const apiKey = import.meta.env.VITE_API_KEY
-  const [movie, setMovie] = useState(null)
   const [movieList, setMovieList] = useState([])
+  
 
   async function getMovie(search) {
+    if (search === undefined) {
+        return
+    }
     try {
       const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${search}`)
       const movieData = await response.json()
   
-      setMovie(movieData)
+      setMovieList([...movieList, movieData])
     } catch (error) {
       console.error({ error: error.message })
     }
