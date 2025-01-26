8e1263a26c3b019b07a6722d3b70a6d0c9852262
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-26 18:27:47 -0500

    updated movie card component

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,27 +1,14 @@
-import React from 'react'
 import { Card } from 'react-bootstrap'
 
 function MovieCard({ movie }) {
-    function loaded() {
-        return (
+    return (
+        <>
             <Card className='bg-dark text-white text-center' border="light" style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={movie.Poster} alt={movie.Title} />
                 <Card.Body>
                     <Card.Title>{movie.Title}</Card.Title>
                 </Card.Body>
             </Card>
-        )
-    }
-
-    function loading() {
-        return (
-            <h1>loading movie card...</h1>
-        )
-    }
-    
-    return (
-        <>
-            { movie? loaded() : loading() }
         </>
     )
 }
